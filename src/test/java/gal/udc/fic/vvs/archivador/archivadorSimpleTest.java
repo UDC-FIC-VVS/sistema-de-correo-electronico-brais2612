ad3f11b4dec883bf997f1416e803ee93348a0af2
Brais Perez <b.perez.busto@udc.es>
2021-01-10 18:20:09 +0100

    Updated: Test ArchivadorSimple & PBT

--- src/test/java/gal/udc/fic/vvs/archivador/archivadorSimpleTest.java
+++ src/test/java/gal/udc/fic/vvs/archivador/archivadorSimpleTest.java
@@ -1,20 +1,27 @@
 package gal.udc.fic.vvs.archivador;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 import org.junit.Test;
 
 import gal.udc.fic.vvs.email.archivador.ArchivadorSimple;
+import gal.udc.fic.vvs.email.archivo.Texto;
+import gal.udc.fic.vvs.email.correo.Mensaje;
 
 
 //Nivel de la prueba:Unidade
-//Categoría: Funcional dinámica de caja negra , positiva
+//Categoría: Funcional dinámica de caja negra 
 //Mecanismo de seleccion de datos: String/int random que se me ha ocurrido.
 
 public class archivadorSimpleTest {
 	
 	private String nombre = "archivador";
 	private int espacio = 5000;
+	
+	
+	//Comprobar:obtenerNombre
+	//Categoría: Funcional dinámica de caja negra positiva
 	
 	@Test
 	public void obtenerNombreTest() {
@@ -23,12 +30,18 @@ public class archivadorSimpleTest {
 		assertEquals(nombre,archivadorTest.obtenerNombre());
 	}
 	
+	//Comprobar:obtenerEspacioTotal
+	//Categoría: Funcional dinámica de caja negra  positiva
+	
 	@Test
 	public void obtenerEspacioTotalTest() {
 		ArchivadorSimple archivadorTest = new ArchivadorSimple(nombre,espacio);
 		
 		assertEquals(espacio,archivadorTest.obtenerEspacioTotal());
 	}
+	
+	//Comprobar:obtenerEspacioDisponible
+	//Categoría: Funcional dinámica de caja negra positiva
 	
 	@Test
 	public void obtenerEspacioDisponibleTest() {
@@ -37,11 +50,42 @@ public class archivadorSimpleTest {
 		assertEquals(espacio,archivadorTest.obtenerEspacioDisponible());
 	}
 	
+	//Comprobar:obtenerDelegado null
+	//Categoría: Funcional dinámica de caja negra positiva
 	@Test
 	public void obtenerDelegadoTest() {
 		ArchivadorSimple archivadorTest = new ArchivadorSimple(nombre,espacio);
 		
 		assertEquals(null,archivadorTest.obtenerDelegado());
+	}
+	
+	//Comprobar: almacenar un correo con espacio vacío
+	//Categoría: Funcional dinámica de caja negra negativa
+	//BUG: No debería dejar con el valor del espacio del archivador siendo 0.
+	
+	@Test 
+	public void almacenarCorreoVacio() {
+		
+		ArchivadorSimple archivadorTest = new ArchivadorSimple(nombre, 0);
+			
+		Mensaje correo = new Mensaje(new Texto("PruebaPruebaPrueba", "PruebaPruebaPruebaPruebaPrueba"));
+				
+		assertFalse(archivadorTest.almacenarCorreo(correo));
+		
+	}
+	
+	//Comprobar almacenar correo
+	//Categoría: Funcional dinámica de caja negra negativa
+	//BUG: Si el archivo tiene el mismo tamaño no se guarda.
+	@Test 
+	public void almacenarCorreoConEspacioigualTamaño() {
+		
+		ArchivadorSimple archivadorTest = new ArchivadorSimple(nombre, 1);
+			
+		Mensaje correo = new Mensaje(new Texto("1", "1"));
+				
+		assertFalse(archivadorTest.almacenarCorreo(correo));
+		
 	}
 
 
--- src/test/java/gal/udc/fic/vvs/archivador/archivadorSimpleTestPBT.java
+++ src/test/java/gal/udc/fic/vvs/archivador/archivadorSimpleTestPBT.java
@@ -1,6 +1,7 @@
 package gal.udc.fic.vvs.archivador;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.runner.RunWith;
@@ -16,10 +17,10 @@ import gal.udc.fic.vvs.email.correo.Mensaje;
 
 
 //Nivel de la prueba: Unidade
-//Categoría: Funcional dinámica de caja negra , positiva
+//Categoría: Funcional dinámica de caja negra 
 //Mecanismo de seleccion de datos: GeneradorString.class 
 
-//ASK: NO SÉ POR QUÉ ME DA UN ERROR LA CONSOLA, PERO EL JUNIT SE EJECUTA BIEN. PREGUNTAR A PROFE
+
 
 @RunWith(JUnitQuickcheck.class)
 
@@ -28,15 +29,80 @@ public class archivadorSimpleTestPBT {
 	private String nombre = "archivador";
 
 
+	//Comprobar Almacenar correo
+	//Categoría: Funcional dinámica de caja negra positiva
+	
 	@Property
-	public void almacenarCorreoTest(@From(GeneradorString.class) String contenido,@InRange(min = "0") int espacio) {
+	public void almacenarCorreoRandomTest(@From(GeneradorString.class) String contenido,@InRange(min = "0") int espacio) {
 		
 		ArchivadorSimple archivadorSimple = new ArchivadorSimple(nombre,espacio);
 		
-		Mensaje correo = new Mensaje(new Texto("TextoPrueba",contenido));
+		Mensaje correo = new Mensaje(new Texto("PruebaPrueba",contenido));
 		
 		assertTrue(archivadorSimple.almacenarCorreo(correo));
-		assertEquals(espacio-correo.obtenerTamaño(),archivadorSimple.obtenerEspacioDisponible());
 		
+	}
+	
+	//Comprobar obtenerNombre 
+	//Categoría: Funcional dinámica de caja negra positiva
+	
+	@Property
+	public void obtenerNombreArchivadorSimple(@From(GeneradorString.class) String nombre) {
+		
+		ArchivadorSimple archivadorSimple = new ArchivadorSimple(nombre, 1);
+		
+		assertEquals(nombre, archivadorSimple.obtenerNombre());
+	}
+	
+	//Comprobar ObtenerEspacioDisponible
+	//Categoría: Funcional dinámica de caja negra positiva
+	
+	@Property 
+	public void almacenarCorreoTest_obtenerEspacioDisponible(@From(GeneradorString.class) String contenido,  @InRange(min = "1") int espacio) {
+		
+		ArchivadorSimple archivadorSimple = new ArchivadorSimple(nombre, espacio);
+		
+		Mensaje correo = new Mensaje(new Texto("TextoPrueba", contenido));
+		
+		archivadorSimple.almacenarCorreo(correo);
+
+		assertEquals(espacio - correo.obtenerTamaño(),
+				archivadorSimple.obtenerEspacioDisponible());
+	}
+	
+	//Comprobar obtenerEspacioTotal
+	//Categoría: Funcional dinámica de caja negra positiva
+	
+	@Property
+	public void obtenerEspacioTotal(int espacio) {
+		
+		ArchivadorSimple archivadorSimple = new ArchivadorSimple(nombre, espacio);
+		
+		assertEquals(archivadorSimple.obtenerEspacioTotal(), espacio);
+	}
+
+	//Comprobar obtenerEspacioDisp  sin archivos
+	//Categoría: Funcional dinámica de caja negra positiva
+	
+	@Property
+	public void obtenerEspacioDisponibleSinAchivo(int espacio) {
+		
+		ArchivadorSimple archivadorSimple = new ArchivadorSimple(nombre, espacio);
+		
+		assertEquals(archivadorSimple.obtenerEspacioDisponible(), espacio);
+	}
+	
+	//Comprobar almacenar con espacio negativo
+	//Categoría: Funcional dinámica de caja negra negativa.
+	
+	//BUG: No deberia dejar poner espacios negativos
+	@Property 
+	public void almacenarConEspacioNegativo(@InRange(max = "-1") int espacio) {
+		
+		ArchivadorSimple archivadorSimple = new ArchivadorSimple(nombre, espacio);
+			
+		Mensaje correo = new Mensaje(new Texto("PruebaPruebaPrueba", "PruebaPruebaPruebaPruebaPrueba"));
+				
+		assertFalse(archivadorSimple.almacenarCorreo(correo));
 	}
 }
